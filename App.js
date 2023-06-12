549a397f42bd30c82f8853383674be248a6b9ff8
dave-lence <davidfnwudo@gmail.com>
2023-06-12 23:59:06 +0100

    project skeleton

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+import LandingScreen from './Screens/LandingScreen';
+
+export default function App() {
+  return (
+    <View>
+      <LandingScreen/>
+    </View>
+  );
+}
+
+
